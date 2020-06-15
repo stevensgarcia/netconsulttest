ebca73351fa5b6c9ed6a8d157046f3fea2711861
Stevens Garcia <smalmsteeng@gmail.com>
2020-06-15 15:50:56 -0500

    Adds solution to example-1.js

--- example-1.js
+++ example-1.js
@@ -4,6 +4,67 @@ const companies = createAll();
 cleanConsole(1, companies);
 console.log('---- EXAMPLE 1 --- ', 'Put here your function');
 
+function solution1(companies) {
+  replaceUndefinedValues(companies);
+  capitalizeStrings(companies);
+  companies.sort(compareUsers);
+  sortUsers(companies);
+};
+
+function replaceUndefinedValues(companies) {
+  companies.forEach((company) => {
+    company.users.forEach((user) => {
+      for (const key of Object.keys(user)) {
+        if (user[key] === undefined) {
+          user[key] = '';
+        }
+      }
+    });
+  });
+};
+
+function capitalizeStrings(companies) {
+  companies.forEach((company) => {
+    company.name = capitalize(company.name);
+    company.users.forEach((user) => {
+      user.firstName = capitalize(user.firstName);
+      user.lastName = capitalize(user.lastName);
+    });
+  });
+};
+
+function capitalize(value) {
+  return (value !== '') ? value[0].toUpperCase() + value.slice(1) : value;
+};
+
+function compareUsers(a, b) {
+  let comparison = 0;
+  if (a.usersLength > b.usersLength) {
+    comparison = 1;
+  } else if (a.usersLength < b.usersLength) {
+    comparison = -1;
+  }
+  return comparison * -1;
+};
+
+function sortUsers(companies) {
+  companies.forEach((company) => {
+    company.users.sort(compareUsersNames);
+  });
+};
+
+function compareUsersNames(a, b) {
+  let comparison = 0;
+  if (a.firstName > b.firstNameb) {
+    comparison = 1;
+  } else if (a.firstName < b.firstName) {
+    comparison = -1;
+  }
+  return comparison;
+};
+
+solution1(companies);
+
 // -----------------------------------------------------------------------------
 // INSTRUCCIONES EN ESPAÑOL
 
